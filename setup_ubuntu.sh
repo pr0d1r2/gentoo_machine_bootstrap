328d03dcfc01a512d1bfce812aeeecc1661d69c3
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-25 03:55:44 +0100

    Unify local ssh key creation into function

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -30,13 +30,24 @@ function bundler_threads() {
   expr `cpu_num` \* 4
 }
 
+function setup_local_ssh_key() {
+  if [ ! -f $HOME/.ssh/id_rsa_$1 ]; then
+    echorun ssh-keygen -b 4096 -f $HOME/.ssh/id_rsa_$1 -C $1@`hostname` -o -a 500 || return $?
+  fi
+
+  if [ ! -d $D_R/chefrepo/site-cookbooks/gentoo_machine_bootstrap/files/default ]; then
+    mkdir -p $D_R/chefrepo/site-cookbooks/gentoo_machine_bootstrap/files/default || return $?
+  fi
+  cp ~/.ssh/id_rsa_$1.pub $D_R/chefrepo/site-cookbooks/gentoo_machine_bootstrap/files/default/id_rsa_$1.pub || return $?
+  ssh-add ~/.ssh/id_rsa_$HOSTNAME || return $?
+}
+
 cd $D_R
 bundle install -j `bundler_threads` || return $?
 
 if [ ! -f $D_R/nodes/$HOSTNAME.setup-done ]; then
-  if [ ! -f $HOME/.ssh/id_rsa_$HOSTNAME ]; then
-    echorun ssh-keygen -b 4096 -f $HOME/.ssh/id_rsa_$HOSTNAME -C $HOSTNAME@`hostname` -o -a 500 || return $?
-  fi
+  setup_local_ssh_key $HOSTNAME || return $?
+
   ssh ubuntu@$HOSTNAME 'mkdir ~/.ssh/'
   echorun scp $HOME/.ssh/id_rsa_$HOSTNAME.pub ubuntu@$HOSTNAME:~/.ssh/authorized_keys || return $?
 
@@ -45,13 +56,6 @@ if [ ! -f $D_R/nodes/$HOSTNAME.setup-done ]; then
     echo "Host $HOSTNAME" >> $HOME/.ssh/config
     echo "  IdentityFile ~/.ssh/id_rsa_$HOSTNAME" >> $HOME/.ssh/config
   fi
-
-  ssh-add ~/.ssh/id_rsa_$HOSTNAME
-
-  if [ ! -d $D_R/chefrepo/site-cookbooks/gentoo_machine_bootstrap/files/default ]; then
-    mkdir -p $D_R/chefrepo/site-cookbooks/gentoo_machine_bootstrap/files/default
-  fi
-  cp ~/.ssh/id_rsa_$HOSTNAME.pub $D_R/chefrepo/site-cookbooks/gentoo_machine_bootstrap/files/default/id_rsa_$HOSTNAME.pub
 
   echorun knife solo prepare ubuntu@$HOSTNAME || return $?
 
