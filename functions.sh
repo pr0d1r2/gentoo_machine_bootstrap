73c931fc970638b3485a7671aae15ed653043299
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-26 18:49:33 +0100

    Add OpenSSH requirement of miniumum 6.5 for '-o' version

--- functions.sh
+++ functions.sh
@@ -23,8 +23,41 @@ function bundler_threads() {
   expr `cpu_num` \* 4
 }
 
+function update_openssh() {
+  case $UNAME in
+    Darwin)
+      brew update || return $?
+      which ssh 2>/dev/null | grep -q "/usr/local/bin/ssh"
+      if [ $? -eq 0 ]; then
+        brew upgrade openssh || return $?
+      else
+        brew install openssh || return $?
+      fi
+      ;;
+    *)
+      echo "Please update your OpenSSH version to minimum of 6.5"
+      exit 3003
+      ;;
+  esac
+}
+
 function setup_local_ssh_key() {
   if [ ! -f $HOME/.ssh/id_rsa_$1 ]; then
+    ssh -V | grep -q "^OpenSSH"
+    if [ $? -eq 0 ]; then
+      local setup_local_ssh_key_SSH_VERSION_MAJOR=`ssh -V | cut -f 1 -d , | cut -f 2 -d _ | cut -f 1 -d .`
+      local setup_local_ssh_key_SSH_VERSION_MINOR=`ssh -V | cut -f 1 -d , | cut -f 2 -d _ | cut -f 2 -d . | cut -f 1 -d p`
+      if [ $setup_local_ssh_key_SSH_VERSION_MAJOR -eq 6 ]; then
+        if [ $setup_local_ssh_key_SSH_VERSION_MINOR -lt 5 ]; then
+          update_openssh || exit $?
+        fi
+      elif [ $setup_local_ssh_key_SSH_VERSION_MAJOR -lt 6 ]; then
+        update_openssh || exit $?
+      fi
+    else
+      echo "There is no openssh!"
+      return 2002
+    fi
     echorun ssh-keygen -b 4096 -f $HOME/.ssh/id_rsa_$1 -C $1@`hostname` -o -a 500 || return $?
   fi
 
