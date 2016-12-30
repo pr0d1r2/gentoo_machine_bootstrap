d7ba23bebfa151e624d6db99ba4fbf04a3139335
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-30 10:43:52 +0100

    Setup ssh keys for hostname

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,10 @@ source $D_R/functions.sh || exit $?
 
 THE_HOSTNAME="127.0.0.1"
 
-setup_local_ssh_key $THE_HOSTNAME || exit $?
-cp ~/.ssh/id_rsa_$THE_HOSTNAME.pub ~/.ssh/authorized_keys || exit $?
+setup_local_ssh_key default || exit $?
+cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || exit $?
+
+cp ~/.ssh/id_rsa_default ~/.ssh/id_rsa_$THE_HOSTNAME || exit $?
+cp ~/.ssh/id_rsa_default.pub ~/.ssh/id_rsa_$THE_HOSTNAME.pub || exit $?
 
 bash ./setup_ubuntu.sh $THE_HOSTNAME || exit $?
