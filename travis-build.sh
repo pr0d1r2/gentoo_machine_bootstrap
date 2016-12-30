c64984e20eca739efe3e5f4ab205f850282007ba
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-30 09:40:17 +0100

    Remove redundant ruby version set

--- travis-build.sh
+++ travis-build.sh
@@ -8,6 +8,4 @@ source $D_R/functions.sh || exit $?
 setup_local_ssh_key default || exit $?
 cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || exit $?
 
-rvm use 2.3.2 || exit $?
-
 bash ./setup_ubuntu.sh 127.0.0.1 || exit $?
