4267ba9f8900cc660ba4dff5a73058b1da9fbe08
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-30 08:38:22 +0100

    Properly return errors from script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -6,20 +6,20 @@ HOSTNAME=$1
 case $HOSTNAME in
   "")
     echo "You must give hostname as first param!!!"
-    return 8472
+    exit 8472
     ;;
 esac
 
-source $D_R/functions.sh || return $?
+source $D_R/functions.sh || exit $?
 
 cd $D_R
-bundle install -j `bundler_threads` || return $?
+bundle install -j `bundler_threads` || exit $?
 
 if [ ! -f $D_R/nodes/$HOSTNAME.setup-done ]; then
-  setup_local_ssh_key $HOSTNAME || return $?
+  setup_local_ssh_key $HOSTNAME || exit $?
 
   ssh ubuntu@$HOSTNAME 'mkdir ~/.ssh/'
-  echorun scp $HOME/.ssh/id_rsa_$HOSTNAME.pub ubuntu@$HOSTNAME:~/.ssh/authorized_keys || return $?
+  echorun scp $HOME/.ssh/id_rsa_$HOSTNAME.pub ubuntu@$HOSTNAME:~/.ssh/authorized_keys || exit $?
 
   cat $HOME/.ssh/config | grep -q "^Host $HOSTNAME$"
   if [ $? -gt 0 ]; then
@@ -27,9 +27,9 @@ if [ ! -f $D_R/nodes/$HOSTNAME.setup-done ]; then
     echo "  IdentityFile ~/.ssh/id_rsa_$HOSTNAME" >> $HOME/.ssh/config
   fi
 
-  echorun knife solo prepare ubuntu@$HOSTNAME || return $?
+  echorun knife solo prepare ubuntu@$HOSTNAME || exit $?
 
   touch $D_R/nodes/$HOSTNAME.setup-done
 fi
 
-echorun knife solo cook ubuntu@$HOSTNAME || return $?
+echorun knife solo cook ubuntu@$HOSTNAME || exit $?
