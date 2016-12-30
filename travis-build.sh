b0d3c607dcb8ca47886b7f48c003b7c414ec2e98
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-30 07:33:16 +0100

    Use exit instead of return to exit from script context

--- travis-build.sh
+++ travis-build.sh
@@ -3,9 +3,9 @@
 D_R=`cd \`dirname $0\` ; pwd -P`
 export PATH="$D_R:$PATH"
 
-source $D_R/functions.sh || return $?
+source $D_R/functions.sh || exit $?
 
 setup_local_ssh_key default || exit $?
-cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || return $?
+cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || exit $?
 
-sh ./setup_ubuntu.sh 127.0.0.1 || return $?
+sh ./setup_ubuntu.sh 127.0.0.1 || exit $?
