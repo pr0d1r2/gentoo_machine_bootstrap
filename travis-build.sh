1ddb9816f569e0209b4eb79847ef95c89a4669c3
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-30 10:49:05 +0100

    Be verbose

--- travis-build.sh
+++ travis-build.sh
@@ -7,10 +7,10 @@ source $D_R/functions.sh || exit $?
 
 THE_HOSTNAME="127.0.0.1"
 
-setup_local_ssh_key default || exit $?
-cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || exit $?
+echorun setup_local_ssh_key default || exit $?
+echorun cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || exit $?
 
-cp ~/.ssh/id_rsa_default ~/.ssh/id_rsa_$THE_HOSTNAME || exit $?
-cp ~/.ssh/id_rsa_default.pub ~/.ssh/id_rsa_$THE_HOSTNAME.pub || exit $?
+echorun cp ~/.ssh/id_rsa_default ~/.ssh/id_rsa_$THE_HOSTNAME || exit $?
+echorun cp ~/.ssh/id_rsa_default.pub ~/.ssh/id_rsa_$THE_HOSTNAME.pub || exit $?
 
-bash ./setup_ubuntu.sh $THE_HOSTNAME || exit $?
+echorun bash ./setup_ubuntu.sh $THE_HOSTNAME || exit $?
