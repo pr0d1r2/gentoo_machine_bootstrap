17557e31e9bb55747f4a2a38f472990f6be38817
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-11 08:08:43 +0100

    Use gentoo_converge for part of chroot configuration

--- chefrepo/site-cookbooks/gentoo_machine_bootstrap/recipes/chroot_converge.rb
+++ chefrepo/site-cookbooks/gentoo_machine_bootstrap/recipes/chroot_converge.rb
@@ -1,7 +1,6 @@
 git '/mnt/gentoo/root/gentoo_converge' do
-  repository 'git@github.com:pr0d1r2/gentoo_converge.git'
+  repository 'https://github.com/pr0d1r2/gentoo_converge.git'
+  enable_submodules false # not need as we do not need extra private configuration
 end
 
-execute 'cp -rvdp /var/chef/node.json /mnt/gentoo/root/gentoo_coverge/node.json'
-
-execute 'chroot /mnt/gentoo /root/gentoo_coverge/setup-on-gentoo.sh'
+execute 'chroot /mnt/gentoo bash /root/gentoo_converge/setup-on-gentoo.sh'
