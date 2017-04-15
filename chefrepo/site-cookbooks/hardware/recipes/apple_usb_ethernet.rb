5c638ef0607e0197025c3122c580beb9dc02e07c
Marcin Nowicki <pr0d1r2@gmail.com>
2017-04-15 08:31:29 +0200

    Add hardware support for Samsung N150 and Apple USB Ethernet

---
+++ chefrepo/site-cookbooks/hardware/recipes/apple_usb_ethernet.rb
@@ -0,0 +1,2 @@
+kernel_enable 'CONFIG_USB_USBNET',
+              'CONFIG_USB_NET_AX8817X'
---
+++ chefrepo/site-cookbooks/hardware/recipes/samsung_n150.rb
@@ -0,0 +1,3 @@
+# Marvell Yukon 2
+kernel_enable 'CONFIG_NET_VENDOR_MARVELL',
+              'CONFIG_SKY2'
