fd27e5291d242173bb1381f101dffc4666a9327c
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-11 06:29:13 +0100

    Conform to shellcheck

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-D_R=`cd \`dirname $0\` ; pwd -P`
-HOSTNAME=$1
-USERNAME=$2
+D_R=$(cd "$(dirname "$0")" || exit 1; pwd -P)
+HOSTNAME="$1"
+USERNAME="$2"
 
 case $HOSTNAME in
   "")
@@ -15,31 +15,30 @@ case $USERNAME in
     USERNAME="ubuntu"
     ;;
 esac
-source $D_R/functions.sh || exit $?
+# shellcheck disable=SC1090
+source "$D_R/functions.sh" || exit $?
 
-cd $D_R
-bundle install -j `bundler_threads` || exit $?
+cd "$D_R" || exit $?
+bundle install -j "$(nproc)" || exit $?
 
-if [ ! -f $D_R/nodes/$HOSTNAME.setup-done ]; then
-  setup_local_ssh_key $HOSTNAME || exit $?
+if [ ! -f "$D_R/nodes/$HOSTNAME.setup-done" ]; then
+  setup_local_ssh_key "$HOSTNAME" || exit $?
 
-  ssh $USERNAME@$HOSTNAME 'mkdir ~/.ssh/'
-  echorun scp $HOME/.ssh/id_rsa_$HOSTNAME.pub $USERNAME@$HOSTNAME:~/.ssh/authorized_keys || exit $?
+  ssh "$USERNAME@$HOSTNAME" 'mkdir ~/.ssh/'
+  echorun scp "$HOME/.ssh/id_rsa_$HOSTNAME.pub" "$USERNAME@$HOSTNAME:~/.ssh/authorized_keys" || exit $?
 
-  cat $HOME/.ssh/config | grep -q "^Host $HOSTNAME$"
-  if [ $? -gt 0 ]; then
-    echo "Host $HOSTNAME" >> $HOME/.ssh/config
-    echo "  IdentityFile ~/.ssh/id_rsa_$HOSTNAME" >> $HOME/.ssh/config
+  if ! (grep -q "^Host $HOSTNAME$" "$HOME/.ssh/config"); then
+    echo "Host $HOSTNAME" >> "$HOME/.ssh/config"
+    echo "  IdentityFile ~/.ssh/id_rsa_$HOSTNAME" >> "$HOME/.ssh/config"
   fi
 
-  echorun knife solo prepare $USERNAME@$HOSTNAME || exit $?
+  echorun knife solo prepare "$USERNAME@$HOSTNAME" || exit $?
 
-  touch $D_R/nodes/$HOSTNAME.setup-done
+  touch "$D_R/nodes/$HOSTNAME.setup-done"
 fi
 
-ssh $USERNAME@$HOSTNAME which git &>/dev/null
-if [ $? -gt 0 ]; then
-  echorun ssh $USERNAME@$HOSTNAME sudo apt-get install git -y || exit $?
+if ! (ssh "$USERNAME@$HOSTNAME" which git &>/dev/null); then
+  echorun ssh "$USERNAME@$HOSTNAME" sudo apt-get install git -y || exit $?
 fi
 
-echorun knife solo cook $USERNAME@$HOSTNAME || exit $?
+echorun knife solo cook "$USERNAME@$HOSTNAME" || exit $?
