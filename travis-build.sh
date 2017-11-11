b754c0b0b19f7c9230230f73ca9ea912a0e9c655
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-11 06:33:59 +0100

    Conform to ShellCheck

--- travis-build.sh
+++ travis-build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-D_R=`cd \`dirname $0\` ; pwd -P`
+D_R=$(cd "$(dirname "$0")" || exit 1; pwd -P)
 export PATH="$D_R:$PATH"
 
-source $D_R/functions.sh || exit $?
+# shellcheck disable=SC1090
+source "$D_R/functions.sh" || exit $?
 
 THE_HOSTNAME="127.0.0.1"
 
 echorun setup_local_ssh_key default || exit $?
-echorun cp ~/.ssh/id_rsa_default.pub ~/.ssh/authorized_keys || exit $?
+echorun cp "$HOME/.ssh/id_rsa_default.pub" "$HOME/.ssh/authorized_keys" || exit $?
 
-echorun cp ~/.ssh/id_rsa_default ~/.ssh/id_rsa_$THE_HOSTNAME || exit $?
-echorun cp ~/.ssh/id_rsa_default.pub ~/.ssh/id_rsa_$THE_HOSTNAME.pub || exit $?
+echorun cp "$HOME/.ssh/id_rsa_default" "$HOME/.ssh/id_rsa_$THE_HOSTNAME" || exit $?
+echorun cp "$HOME/.ssh/id_rsa_default.pub" "$HOME/.ssh/id_rsa_$THE_HOSTNAME.pub" || exit $?
 
-ssh-keyscan $THE_HOSTNAME >> ~/.ssh/known_hosts || exit $?
+ssh-keyscan "$THE_HOSTNAME" >> "$HOME/.ssh/known_hosts" || exit $?
 
-echorun bash ./setup_ubuntu.sh $THE_HOSTNAME `whoami` || exit $?
+echorun bash ./setup_ubuntu.sh "$THE_HOSTNAME" "$(whoami)" || exit $?
