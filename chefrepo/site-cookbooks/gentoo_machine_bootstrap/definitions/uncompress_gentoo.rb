05f60b4e1a4edbdaf694df3d58b1965e9f70a037
Marcin Nowicki <pr0d1r2@gmail.com>
2018-06-23 17:14:44 +0200

    Use xz compressed stage archive

--- chefrepo/site-cookbooks/gentoo_machine_bootstrap/definitions/uncompress_gentoo.rb
+++ chefrepo/site-cookbooks/gentoo_machine_bootstrap/definitions/uncompress_gentoo.rb
@@ -2,6 +2,6 @@ define :uncompress_gentoo do
   uncompress_gentoo_root = params[:name] || raise('give destination directory as first parameter')
 
   with_marker_file :uncompress_gentoo do
-    execute "tar xfj #{uncompress_gentoo_root}/stage-latest.tar.bz2 -C #{uncompress_gentoo_root}"
+    execute "xz -cd #{uncompress_gentoo_root}/stage-latest.tar.xz | tar xf - -C #{uncompress_gentoo_root}"
   end
 end
