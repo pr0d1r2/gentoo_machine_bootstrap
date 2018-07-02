578f2be0d889747f666e9984f50c1c437c1c4a62
Marcin Nowicki <pr0d1r2@gmail.com>
2018-07-02 02:36:09 +0200

    Fix build of openssl

--- chefrepo/site-cookbooks/gentoo_machine_bootstrap/recipes/chroot_chefdk.rb
+++ chefrepo/site-cookbooks/gentoo_machine_bootstrap/recipes/chroot_chefdk.rb
@@ -1,3 +1,16 @@
+directory '/mnt/gentoo/etc/portage/package.keywords' do
+  recursive true
+end
+
+directory '/mnt/gentoo/etc/portage/package.unmask' do
+  recursive true
+end
+
+execute 'echo "dev-libs/openssl ~amd64" > /mnt/gentoo/etc/portage/package.keywords/openssl'
+execute 'echo "=dev-libs/openssl-1.0.2o-r6" > /mnt/gentoo/etc/portage/package.unmask/openssl'
+
+execute 'USE=-bindist chroot /mnt/gentoo emerge --usepkg openssl openssh'
+
 execute 'chroot /mnt/gentoo emerge --usepkg layman' do
   not_if { system('chroot /mnt/gentoo which layman 1>/dev/null 2>/dev/null') }
 end
@@ -12,13 +25,8 @@ execute 'echo y | chroot /mnt/gentoo layman -a ssnb' do
   not_if { File.directory?(ssnb_path) }
 end
 
-
-directory '/mnt/gentoo/etc/portage/package.keywords' do
-  recursive true
-end
-
 execute 'echo "app-admin/chefdk-omnibus ~amd64" > /mnt/gentoo/etc/portage/package.keywords/chefdk-omnibus'
 
-execute 'chroot /mnt/gentoo emerge --usepkg chefdk-omnibus' do
+execute 'chroot /mnt/gentoo emerge --autounmask-write --usepkg chefdk-omnibus' do
   not_if { system('chroot /mnt/gentoo which chef-solo 1>/dev/null 2>/dev/null') }
 end
